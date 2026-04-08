8cca5abbe4626c32a7a217f92847c425d238c4b5
kgn <kevin.glovernetherton@gmail.com>
2026-04-08 13:28:07 -0400

    Add unusable flag based on parser resposnse, show badge in UI, use validation function across UI when uploading to SD card

--- src/server/controllers/Grblhal/GrblHalLineParserResultSDCard.js
+++ src/server/controllers/Grblhal/GrblHalLineParserResultSDCard.js
@@ -4,24 +4,26 @@ class GrblHalLineParserResultSDCard {
         // [FILE:/Control-Box-test-code.nc|SIZE:66]
         // [FILE:/._Control-Box-test-code.nc|SIZE:4096]
         // [FILE:/SLB-o-christmas-tree.nc|SIZE:5580]
-        const r = line.match(/\[FILE:\/([^|]+)\|SIZE:(\d+)\]/gm);
+        const r = line.match(/\[FILE:\/([^|]+)\|SIZE:(\d+)(\|UNUSABLE)?\]/gm);
 
         if (!r) {
             return null;
         }
 
-        // Extract FILE and SIZE from the match
-        const fileMatch = r[0].match(/\[FILE:\/([^|]+)\|SIZE:(\d+)\]/);
+        // Extract FILE, SIZE, and optional UNUSABLE flag from the match
+        const fileMatch = r[0].match(/\[FILE:\/([^|]+)\|SIZE:(\d+)(\|UNUSABLE)?\]/);
         const name = fileMatch ? fileMatch[1] : '';
 
         if (name.startsWith('._')) {
             return null; // hide mac dot hidden files
         }
         const size = fileMatch ? parseInt(fileMatch[2], 10) : 0;
+        const unusable = fileMatch ? !!fileMatch[3] : false;
 
         const payload = {
             name,
-            size
+            size,
+            unusable
         };
 
         return {
